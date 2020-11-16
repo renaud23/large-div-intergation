422cf21c4695f40c03529be5a62fa3a5aaab842b
Renaud Genevois <KQHLZ2@ad.insee.intra>
2020-11-16 16:20:11 +0100

    with touch

--- src/components/tables/editable-table.js
+++ src/components/tables/editable-table.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { ReactLargeTable } from "react-scrollable-div";
+import platform from "platform-detect";
+import classnames from "classnames";
 import { generatePersonData } from "./random-data";
+
 import "./light-theme.scss";
 
 const NR_ROWS = 10000;
-
 function Table() {
   return (
     <ReactLargeTable
       data={generatePersonData(NR_ROWS)}
       headerHeight={50}
-      className="light-theme"
+      className={classnames("light-theme", { touch: platform.touch })}
       rowNums={false}
       rowHeight={40}
       treeSize={false}
